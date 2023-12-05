f788b75ee0ca2f05436ba6ab1c5bec2bb98ca5c7
Congyu <leonwang998@gmail.com>
2023-12-05 10:48:04 +0800

    revert exposing CSlice

--- src/lib.rs
+++ src/lib.rs
@@ -121,7 +121,7 @@ pub use crate::{
     },
     db_pinnable_slice::DBPinnableSlice,
     env::Env,
-    ffi_util::{CSlice, CStrLike},
+    ffi_util::CStrLike,
     iter_range::{IterateBounds, PrefixRange},
     merge_operator::MergeOperands,
     perf::{PerfContext, PerfMetric, PerfStatsLevel},
